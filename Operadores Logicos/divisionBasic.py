5927e6fddc3d61ba0b929361b90b9f8382c4c891
Jhon Jairo Linares <67304819+jhonjlinaresb@users.noreply.github.com>
2022-02-28 22:14:46 +0100

    Delete duplicate operadores logicos

--- Operadores Logicos/divisionBasic.py
+++
@@ -1,9 +0,0 @@
-#Dividir Dos números
-
-numeroUno = 49
-numeroDos = 2
-
-resultado = (numeroUno / numeroDos)
-#resultado = (numeroUno // numeroDos)
-print("El resultado de la division es: ")
-print(resultado)
