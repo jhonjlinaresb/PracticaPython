2a3f070054e929bed65608eb53f64dd68421d78a
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-02-28 22:03:15 +0100

    Operadores Logicos Python

---
+++ Operadores Logicos/divisionBasic.py
@@ -0,0 +1,9 @@
+#Dividir Dos números
+
+numeroUno = 49
+numeroDos = 2
+
+resultado = (numeroUno / numeroDos)
+#resultado = (numeroUno // numeroDos)
+print("El resultado de la division es: ")
+print(resultado)
---
+++ Operadores Logicos/restaBasic.py
@@ -0,0 +1,12 @@
+#Operadores Lógicos - RESTA
+
+'''
+Crear un programa que realice la resta de dos números
+'''
+numUno = 73
+numDos = 32
+
+resultado = (numUno - numDos)
+
+print("El resultado de la resta es: ")
+print(resultado)
