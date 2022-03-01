49dfa80b4b5fe9d9a275024b0274a62b91301f7e
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-03-01 21:29:45 +0100

    Crear OpLogicos and mejorar condicionesAnidadas

--- Condiciones/condicionesAnidadas.py
+++ Condiciones/condicionesAnidadas.py
@@ -2,13 +2,13 @@
 Se usará if anidado'''
 
 edad = int(input("Ingrese su edad: ")) #edad es una variable de tipo entero, pide una edad al usuario con INPUT
-pago = bool(True) #pago es una variable de tipo booleano
+pago = bool(False) #pago es una variable de tipo booleano
 
 if (edad >= 18): #Si la edad es mayor o igual a 18,
     if (pago == True): #Si el pago es verdadero, entonces:
-        print("Es mayor de edad y puede pasar") #Imprime: "Es mayor de edad y puede pasar"
+        print("Es mayor de edad y puede pasar porque el pago es verdadero") #Imprime: "Es mayor de edad y puede pasar"
     else: #Si el pago es falso, entonces:
-        print("Es mayor de edad y no puede pasar") #Imprime: "Es mayor de edad y no puede pasar"
+        print("Es mayor de edad y no puede pasar porque el pago es falso") #Imprime: "Es mayor de edad y no puede pasar"
 else: #Si la edad es menor a 18, entonces:
     print("Es menor de edad") #Imprime: "Es menor de edad"
 
---
+++ Condiciones/operadoresLogicos.py
@@ -0,0 +1,9 @@
+'''Practicaremos los operadores logicos en este ejercicio
+and, or, not, is, is not, in, not in
+y, o, no, es, no es, en, no en'''
+
+edad = int(20)
+if (edad >= 18 and edad < 30):
+    print("Su edad es mayor a 18 y menor a 30")
+else:
+    print("Su edad no es mayor a 18 y menor a 30")
