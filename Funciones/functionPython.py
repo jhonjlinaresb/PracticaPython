015763f244d13028138342494abf52d22f1ae1b4
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-03-02 20:24:26 +0100

    Ejemplo de función en Python

---
+++ Funciones/functionPython.py
@@ -0,0 +1,16 @@
+#Ejemplo de como realizar una función en python:
+'''Forma de acomodar el código poor bloques de código
+def nombreFuncion(parametros):
+    #Código
+    # ...'''
+
+def dev():
+    print("Jhon Developer")
+
+def suma(a, b):
+    print(a+b)
+    #return a + b
+
+dev()
+suma(4,6)
+print(suma)
