9b9c07b8f7f3cd37fbb165d674f49c11c0f12b85
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-03-02 19:24:12 +0100

    Ejemplo de condicion while

---
+++ Condiciones/whileEjemplos.py
@@ -0,0 +1,9 @@
+#Creamos un programa que no caiga en un bucle infinito
+
+numerillo = int(1)
+
+while(numerillo <= 10): #Mientras el numero sea menor o igual a 10, seguirá ejecutando el código
+    print("El valor del numero es: ", numerillo) #Imprime el valor del numero
+    numerillo = numerillo + 1 #Incrementa el numero en 1
+
+'''ToDo: Realizar un programa que mientras esté lloviendo, el usuario lleve paraguas'''
