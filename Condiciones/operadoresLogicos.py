09dbc8aba340e6c79df99d836f5173eb76374f72
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-03-02 18:08:45 +0100

    Ejemplo de operadores logicos usando if anidados

--- Condiciones/operadoresLogicos.py
+++ Condiciones/operadoresLogicos.py
@@ -2,8 +2,24 @@
 and, or, not, is, is not, in, not in
 y, o, no, es, no es, en, no en'''
 
+#Ejercicio 1:
 edad = int(20)
-if (edad >= 18 and edad < 30):
+
+if (edad >= 18 and edad < 30): #Debe cumplir con ambas condiciones
     print("Su edad es mayor a 18 y menor a 30")
 else:
     print("Su edad no es mayor a 18 y menor a 30")
+
+#Ejercicio 2:
+
+pago = int(25)
+valorEntrada = int(25)
+if (pago >= valorEntrada or pago == valorEntrada): #Debe cumplir con alguna de las condiciones
+    print("Puede pasar al teatro")
+    if (pago != valorEntrada): #Si es diferente al valor de la entrada
+        cantidadDevuelta = int(pago - valorEntrada)
+        print("Le devolvemos €" + str(cantidadDevuelta)) #Convierte el valor de cantidadDevuelta a string
+    else:
+        print("No le devolvemos nada") #Si el pago es igual al valor de la entrada
+else:
+    print("No puede pasar al teatro porque no ha pagado la cantidad de entrada")
