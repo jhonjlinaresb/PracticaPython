186a4d529412f520d65c9ce20920753faeaba3d8
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-03-04 23:17:27 +0100

    Comentar ejemplo de uso en generar tablas

--- Retos/generarTablas.py
+++ Retos/generarTablas.py
@@ -10,3 +10,19 @@ def tabla(numero, limite):
         print(f'{numero} x {f} = {generar}') #Imprime la tabla de multiplicar
 
 tabla(numero, limite)
+
+''' Ejemplo de uso:
+-----Inicio-----
+Ingrese un numero: 5
+Ingrese el limite: 10
+5 x 0 = 0
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+-----Fin--------'''
