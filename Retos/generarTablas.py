28665f5b42628e37edff15edd8400b525fd20171
Jhon Jairo Linares <jhonjlinaresb@gmail.com>
2022-03-04 23:14:34 +0100

    Crear programa generar tablas de multiplicar for loop

--- Retos/generarTablas.py
+++ Retos/generarTablas.py
@@ -3,11 +3,10 @@ ToDo: For Loop'''
 
 numero = int(input("Ingrese un numero: "))
 limite = int(input("Ingrese el limite: "))
-texto = ""
+#-Crear un for loop que imprima la tabla de multiplicar del numero y limite ingresado
 def tabla(numero, limite):
-    for numero in range(limite):
-        print(texto * limite)
-        numero = numero + 1
-        print(numero * limite)
+    for f in range(0, limite):
+        generar = numero * f #Genera la tabla de multiplicar
+        print(f'{numero} x {f} = {generar}') #Imprime la tabla de multiplicar
 
 tabla(numero, limite)
